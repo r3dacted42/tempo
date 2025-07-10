5ccd68c2f29979a8dd18a92def78a91ca2d43fee
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2025-07-11 04:05:13 +0530

    donut

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import { OrbitControls } from '@tresjs/cientos';
+import { TresCanvas } from '@tresjs/core';
 </script>
 
 <template>
+  <TresCanvas clear-color="#82DBC5" window-size>
+    <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" />
+    <TresMesh>
+      <TresTorusGeometry :arga="[1, 0.5, 16, 32]" />
+      <TresMeshBasicMaterial color="orange" />
+    </TresMesh>
+    <TresAmbientLight :intensity="1" />
+    <OrbitControls />
+    <TresGridHelper :args="[10, 10]" />
+  </TresCanvas>
 </template>
-
-<style scoped>
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import Tres from '@tresjs/core'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App)
-    .use(Tres)
-    .mount('#app')
+createApp(App).mount('#app');
